5491fd07fd5db544a1e3c6972999bd315cd468ad
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-10 13:20:51 -0700

    added post request to /api/cars and validateCar middleware.

--- api/routes/carsRouter.js
+++ api/routes/carsRouter.js
@@ -15,6 +15,29 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', validateCar, (req, res) => {
+    const car = req.body;
+
+    db('cars').insert(car)
+    .then(car => {
+        res.status(200).json(car[0]);
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'Car could not be added to database.' });
+    })
+})
+
+
+
+function validateCar (req, res, next) {
+    const { vin, make, model, mileage } = req.body;
+
+    if (!vin || !make || !model || !mileage) {
+        res.status(400).json({ message: 'Please provide all required fields.' });
+    } else {
+        next();
+    }
+}
 
 
 
