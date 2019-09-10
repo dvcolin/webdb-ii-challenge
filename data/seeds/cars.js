f6e6f0065bb255a53ef23dee34f684cbac9d3c2d
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-10 13:29:07 -0700

    updated table to include transmission and status. created new migration. Completed MVP

--- data/seeds/cars.js
+++ data/seeds/cars.js
@@ -6,10 +6,10 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('cars').insert([
         {vin: '1G1ZT51FX6F111393', make: 'Chevrolet', model: 'Malibu', mileage: 45345 },
-        {vin: 'W06VR52R9WR220134', make: 'Cadillac', model: 'Catera', mileage: 45345 },
-        {vin: '1N4BA41E74C829102', make: 'Nissan', model: 'Maxima', mileage: 45345 },
+        {vin: 'W06VR52R9WR220134', make: 'Cadillac', model: 'Catera', mileage: 45345, transmission: 'manual' },
+        {vin: '1N4BA41E74C829102', make: 'Nissan', model: 'Maxima', mileage: 45345, status: 'clean' },
         {vin: 'JH4KA4660LC006262', make: 'Acura', model: 'Legend', mileage: 45345 },
-        {vin: 'KMHFG4JG1CA181127', make: 'Hyundai', model: 'Azera', mileage: 45345 },
+        {vin: 'KMHFG4JG1CA181127', make: 'Hyundai', model: 'Azera', mileage: 45345, transmission: 'automatic' },
       ]);
     });
 };
