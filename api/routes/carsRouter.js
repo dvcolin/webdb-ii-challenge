159b3d134dba635906f9635efafbd41648bcfb2f
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-10 12:19:54 -0700

    added carsRouter file. imported carsRouter into server.js. set up db-config. added first migration

---
+++ api/routes/carsRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const db = require('../../data/db-config.js');
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+
+})
+
+
+
+
+module.exports = router;
