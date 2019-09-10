9bdb366de8f4f15468318e010722d34faf414168
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-10 12:32:45 -0700

    set up knex migration and created table with columns and constraints

--- api/routes/carsRouter.js
+++ api/routes/carsRouter.js
@@ -5,7 +5,14 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-
+    db('cars')
+    .then(cars => {
+        res.status(200).json(cars);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+        console.log(err)
+    })
 })
 
 
--- data/migrations/20190910121322_starter.js
+++ data/migrations/20190910121322_starter.js
@@ -1,8 +1,23 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', table => {
+      // id, primary key
+      table.increments();
+
+      //VIN number
+      table.string('VIN').unique().notNullable();
+
+      // Make
+      table.string('Make', 128).notNullable();
+
+      //Model
+      table.string('Model', 128).notNullable();
+
+      // Mileage
+      table.integer('Mileage').notNullable();
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
